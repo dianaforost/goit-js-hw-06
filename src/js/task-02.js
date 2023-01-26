7657e0c4dba83ce2e19c53475363978aa81ab9c7
Diana Forostiana <dianaforost@gmail.com>
2023-01-26 13:16:27 +0100

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const markup = ingredients
+  .map((ingredient) => `<li class="item">${ingredient}</li>`)
+  .join("");
+list.innerHTML = markup;
