beb897e8e3634e2047cb9be5347910f6f524d109
Diana Forostiana <dianaforost@gmail.com>
2023-01-30 18:30:02 +0100

    Update task-09,js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorButton = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const body = document.querySelector("body");
+changeColorButton.addEventListener("click", (event)=> {
+body.style.backgroundColor = getRandomHexColor();
+spanEl.textContent = body.style.backgroundColor;
+})
