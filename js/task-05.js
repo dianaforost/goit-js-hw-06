583644d2c2f21a63c2d6ce99c6cda1a8648e858e
Diana Forostiana <dianaforost@gmail.com>
2023-02-01 13:35:55 +0100

    Change task-05,js

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,8 @@
 const inputEl = document.querySelector("#name-input");
 const spanEl = document.querySelector("#name-output");
 inputEl.addEventListener("input", (event) => {
+  if (inputEl.value === "") {
+    return (spanEl.textContent = "Anonymous");
+  }
   spanEl.textContent = event.currentTarget.value;
 });
