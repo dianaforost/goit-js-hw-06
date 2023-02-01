f09550da4c41f27f7f45bdbde151f58f368da409
Diana Forostiana <dianaforost@gmail.com>
2023-02-01 13:52:04 +0100

    Change task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputEl = document.querySelector("#font-size-control");
 const spanEl = document.querySelector("#text");
+spanEl.style.fontSize = inputEl.value + "px";
 inputEl.addEventListener("input", (event) => {
   spanEl.style.fontSize = event.currentTarget.value + "px";
   console.log(spanEl.style.fontSize);
