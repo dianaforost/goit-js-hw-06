fb4b3b8113338fa01b53309c5dcdcb4c17171a95
Diana Forostiana <dianaforost@gmail.com>
2023-02-01 13:38:34 +0100

    Change task-06,js

--- js/task-06.js
+++ js/task-06.js
@@ -5,9 +5,11 @@ const isValid = (event) => {
   if (event.currentTarget.value.length === inputElLength) {
     console.log("Hi");
     inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
     return;
   }
   console.log("By");
   inputEl.classList.add("invalid");
+  inputEl.classList.remove("valid");
 };
 inputEl.addEventListener("blur", isValid);
